c171a1ce91b1285def7a76e4e505bdb6dbe5db88
aswinsivadas18 <aswinsivadas18@gmail.com>
2023-06-12 03:21:45 +0530

    first commit

---
+++ src/Components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Quiz from './Quiz'
+
+const Main = () => {
+  return (
+    <div>
+      <Quiz/>
+    </div>
+  )
+}
+
+export default Main
---
+++ src/Components/Quiz.js
@@ -0,0 +1,78 @@
+
+import React, { useState } from "react";
+import "./style.css";
+import questions from "./Questions";
+
+const Quiz = () => {
+
+  const [showResults, setShowResults] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+
+
+  const optionClicked = (isCorrect) => {
+    // Increment the score
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setShowResults(true);
+    }
+  };
+
+  /* Resets the game back to default */
+  const restartGame = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setShowResults(false);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="text">IQ Gamer</h1>
+      <h2 className="text">Score: {score}</h2>
+
+
+      {showResults ? (
+        /*  Final Results */
+        <div className="final-results">
+          <h1>Final Results</h1>
+          <h2>
+            {score} out of {questions.length} correct - (
+            {(score / questions.length) * 100}%)
+          </h2>
+          <button onClick={() => restartGame()}>Restart game</button>
+        </div>
+      ) : (
+        /* Question Card  */
+        <div className="question-card">
+
+          <h2>
+            Question: {currentQuestion + 1} out of {questions.length}
+          </h2>
+          <h3 className="question-text">{questions[currentQuestion].text}</h3>
+
+
+          <ul>
+            {questions[currentQuestion].options.map((option) => {
+              return (
+                <li
+                  key={option.id}
+                  onClick={() => optionClicked(option.isCorrect)}
+                >
+                  {option.text}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+
+export default Quiz
